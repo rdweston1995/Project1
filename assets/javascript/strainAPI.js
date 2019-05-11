32f4ae69599653c29d8b71f2a63ddffea4f19330
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-11 12:53:41 -0700

    Basic function of searching the API for the search parameters that the user provides working

--- assets/javascript/strainAPI.js
+++ assets/javascript/strainAPI.js
@@ -24,131 +24,10 @@ $(document).ready(function(){
 
     var apiKey = "CHAdUYO"
     
-    var positive = [];
-    var negative = [];
-    var medical = [];
-    var flavors = [];
-
-    var strainArr = [];
-    var flavorArr = [];
-    var effectArr = [];
     var nameArr = [];
+    var flavorNameArr = [];
     var idArr = [];
-    var matchesIdArr = [];
-    var matchesNameArr = [];
-    
-    $.ajax({
-        url: "http://strainapi.evanbusse.com/" + apiKey + "/searchdata/effects",
-        method: 'GET'
-    }).then(function(response){
-        //console.log(response);
-        for(var i = 0; i < response.length; i++){
-            if(response[i].type === "positive"){
-                positive.push(response[i]);
-                
-            }else if(response[i].type === "negative"){
-                negative.push(response[i]);
-            }else if(response[i].type === "medical"){
-                medical.push(response[i]);
-            }
-        }
-    });
 
-    $.ajax({
-        url: "http://strainapi.evanbusse.com/" + apiKey + "/searchdata/flavors",
-        method: 'GET'
-    }).then(function(response){
-        //console.log(reponse);
-        for(var i = 0; i < response.length; i++){
-            flavors.push(response[i]);
-        }
-    });
-
-    var effects = {
-        'positive': positive,
-        'negative': negative,
-        'medical': medical,
-        'flavors': flavors,
-    }
-
-    //Want to save to the database the master list of the different strains so that its only calle once.
-    //When a user selects what effects they are looking for I want to search the database for strains that fit the requirement
-    //When the search button is pressed itll search the database looking for the effects
-
-    //Testing
-    /*
-    $("#submit").on('click', function(){
-        var strain = $(".typeStrains").val().trim();
-        var effect = $(".typeEffects").val().trim();
-        var flavor = $(".typeFlavor").val().trim();
-
-        $.ajax({
-            url: "http://strainapi.evanbusse.com/" + apiKey + "/strains/search/flavor/" + flavor,
-            method: 'GET'
-        }).then(function(response){
-            for(var i = 0; i < response.length; i++){
-                flavorArr.push(response[i].id);
-            }
-            console.log("----- FLAVOR -----");
-            console.log(flavorArr);
-
-        });
-
-        $.ajax({
-            url: "http://strainapi.evanbusse.com/" + apiKey + "/strains/search/effect/" + effect,
-            method: 'GET'
-        }).then(function(response){
-            for(var i = 0; i < response.length; i++){
-                effectArr.push(response[i].id);
-            }
-            console.log("----- EFFECT -----");
-            console.log(effectArr);
-            
-        });
-
-        $.ajax({
-            url: "http://strainapi.evanbusse.com/" + apiKey + "/strains/search/race/" + strain,
-            method: 'GET'
-        }).then(function(response){
-            for(var i = 0; i < response.length; i++){
-                strainArr.push(response[i].id);
-            }
-            console.log("----- STRAIN -----");
-            console.log(strainArr);
-        });
-
-        if(flavorArr.length <= effectArr.length && flavorArr.length <= strainArr.length && flavorArr.length !== 0){
-            console.log("FLAVOR LENGTH: " + flavorArr.length);
-            for(var i = 0; i < flavorArr.length; i++){
-                //if(flavorArr[i] === effectArr[i] && flavorArr[i] === strainArr[i]){
-                //    matchesArr.push(flavorArr[i]);
-                //}
-                console.log(effectsArr.indexOf(flavorArr[i]));
-            }
-            console.log("------ MATCHES ------");
-            console.log(matchesIdArr);
-        }else if(effectArr.length <= flavorArr.length && effectArr.length <= strainArr.length){
-            console.log("EFFECT LENGTH: " + effectArr.length);
-            for(var i = 0; i < effectArr.length; i++){
-                if(effectArr[i] === flavorArr[i] && effectArr[i] === strainArr[i]){
-                    matchesIdArr.push(effectArr[i]);
-                    
-                }
-            }
-            console.log("------ MATCHES ------");
-            console.log(matchesIdArr);
-        }else{
-            console.log("STRAINS LENGHT: " + strainArr.length);
-            for(var i = 0; i < strainArr.length; i++){
-                if(strainArr[i] === flavorArr[i] && strainArr[i] === effectArr[i]){
-                    matchesIdArr.push(strainArr[i]);
-                }
-            }
-            console.log("------ MATCHES ------");
-            console.log(matchesIdArr);
-        }
-    });
-*/
 
     $("#submit").on('click', function(){
         var strain = $(".typeStrains").val().trim();
@@ -165,14 +44,29 @@ $(document).ready(function(){
                     idArr.push(response[i].id);
                 }
             }
-            console.log("-----NAME-----");
-            console.log(nameArr);
-            console.log("-----ID-------");
-            console.log(idArr);
-        }); 
+            $.ajax({
+                url: "http://strainapi.evanbusse.com/" + apiKey + "/strains/search/flavor/" + flavor,
+                method:'GET'
+            }).then(function(response){
+                for(var i = 0; i < response.length; i++){
+                    flavorNameArr.push(response[i].name);
+                }
+                getMatches(flavor);
+            });
+        });     
     });
 
-
-    //console.log(effects);
-    
+    function getMatches(flavor){
+        $.ajax({
+            url: "http://strainapi.evanbusse.com/" + apiKey + "/strains/search/all",
+            method: 'GET'
+        }).then(function(response){
+            for(var i = 0; i < flavorNameArr.length; i++){
+                if(nameArr.indexOf(flavorNameArr[i]) !== -1){
+                    //console.log(flavorNameArr[i]);
+                    console.log(response[flavorNameArr[i]]);
+                }
+            }
+        });  
+    }
 });
