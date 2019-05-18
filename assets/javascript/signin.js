233234d84c3f45602fc3b978bee5ac206d049c86
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-18 10:31:15 -0700

    Finished sign in

--- assets/javascript/signin.js
+++ assets/javascript/signin.js
@@ -42,6 +42,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
+    'credentialHelper': firebaseui.auth.CredentialHelper.NONE,
     signInSuccessUrl: 'main.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
